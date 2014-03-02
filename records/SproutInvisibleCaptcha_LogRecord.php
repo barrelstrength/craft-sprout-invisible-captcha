836bc396c6648477692b046e4dc3d96f9c5a1bb6
Ben Parizek <ben@barrelstrengthdesign.com>
2014-03-02 08:29:37 -0800

    Added protect() Deprecated setCaptcha() Updated all indent spacing to tabs

--- records/SproutInvisibleCaptcha_LogRecord.php
+++ records/SproutInvisibleCaptcha_LogRecord.php
@@ -3,33 +3,33 @@ namespace Craft;
 
 class SproutInvisibleCaptcha_LogRecord extends BaseRecord
 {
-    public function getTableName()
-    {
-        return 'sproutinvisiblecaptcha_log';
-    }
+	public function getTableName()
+	{
+		return 'sproutinvisiblecaptcha_log';
+	}
 
-    protected function defineAttributes()
-    {
-        return array(
-            'timeMethodFailed'      => array(AttributeType::Bool),
-            'honeypotMethodFailed'  => array(AttributeType::Bool),
-            'originMethodFailed'    => array(AttributeType::Bool),
-            'postData'              => array(AttributeType::Mixed),
-            'ipAddress'             => array(AttributeType::String)
-        );
-    }
+	protected function defineAttributes()
+	{
+		return array(
+			'timeMethodFailed'      => array(AttributeType::Bool),
+			'honeypotMethodFailed'  => array(AttributeType::Bool),
+			'originMethodFailed'    => array(AttributeType::Bool),
+			'postData'              => array(AttributeType::Mixed),
+			'ipAddress'             => array(AttributeType::String)
+		);
+	}
 
-    /**
-     * Create a new instance of the current class. This allows us to
-     * properly unit test our service layer.
-     *
-     * @return BaseRecord
-     */
-    public function create()
-    {
-        $class = get_class($this);
-        $record = new $class();
+	/**
+	 * Create a new instance of the current class. This allows us to
+	 * properly unit test our service layer.
+	 *
+	 * @return BaseRecord
+	 */
+	public function create()
+	{
+		$class = get_class($this);
+		$record = new $class();
 
-        return $record;
-    }
+		return $record;
+	}
 }
